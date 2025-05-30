4372aa545dddc5caa5fdefaafe3ebfbf5ef24731
JacoboSolarte <jacobo.solarte@campusucc.edu.co>
2025-05-30 11:06:34 -0500

    2.0

--- GestionReclamos/GestionReclamos/settings.py
+++ GestionReclamos/GestionReclamos/settings.py
@@ -135,9 +135,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Si usas Vite en React
-]
+""" CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",   # Si usas Vite en React
+] """
 
 from pathlib import Path
 
